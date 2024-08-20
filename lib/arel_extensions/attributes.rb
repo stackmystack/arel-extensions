a60b56f09f098eb4c4fc1c128fa37a42c21f9e46
Firas al-Khalil <firas.alkhalil@faveod.com>
2024-08-20 11:54:08 +0200

    rails: 7.2: patch where clause merger to work with our attributes

--- lib/arel_extensions/attributes.rb
+++ lib/arel_extensions/attributes.rb
@@ -18,11 +18,22 @@ module ArelExtensions
     include ArelExtensions::StringFunctions
     include ArelExtensions::Predications
 
+    @@warn_eqeq = true
+    @@warn_noteq = true
+
     def ==(other)
+      if ActiveRecord.version >= Gem::Version.create('7.2') && @@warn_eqeq
+        warn("`==` is now deprecated and will be removed starting from rails 8.0")
+        @@warn_eqeq = false
+      end
       Arel::Nodes::Equality.new self, Arel.quoted(other, self)
     end
 
     def !=(other)
+      if ActiveRecord.version >= Gem::Version.create('7.2') && @@warn_noteq
+        warn("`!=` is now deprecated and will be removed starting from rails 8.0")
+        @@warn_noteq = false
+      end
       Arel::Nodes::NotEqual.new self, Arel.quoted(other, self)
     end
   end
